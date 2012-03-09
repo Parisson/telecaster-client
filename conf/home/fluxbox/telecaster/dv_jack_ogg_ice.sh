9414cd0c1afac5c7dd13d5a57901e8caee1a8311
momo <momo@augustins.pre-barreau.com>
2012-03-09 11:18:55 +0100

    bugfix

--- conf/home/fluxbox/telecaster/dv_jack_ogg_ice.sh
+++ conf/home/fluxbox/telecaster/dv_jack_ogg_ice.sh
@@ -3,12 +3,10 @@
 WIDTH=320
 HEIGHT=240
 
-gst-launch-0.10 dv1394src \ 
-        ! queue ! dvdemux ! queue ! dvdec \
-        ! queue ! videscale ! video/x-raw-yuv, width=$WIDTH, height=$HEIGHT \ 
-        ! queue ! ffmpegcolorspace \
-        ! queue ! theoraenc quality=20 ! queue ! mux. \
-	! jackaudiosrc connect=0 \
-        ! queue ! audioconvert ! queue ! vorbisenc quality=0.3 ! queue ! mux.  \
-	oggmux name=mux ! shout2send mount=/telecaster_live_video.ogg port=8000 password=source2parisson ip=127.0.0.1 
+gst-launch-0.10 dv1394src ! dvdemux ! queue ! dvdec \
+ ! queue ! videoscale ! video/x-raw-yuv,width=$WIDTH,height=$HEIGHT \
+ ! queue ! ffmpegcolorspace ! queue ! theoraenc quality=20 ! queue ! mux. \
+ jackaudiosrc connect=0 ! queue ! audioconvert \
+ ! queue ! vorbisenc quality=0.3 ! queue ! mux. \
+ oggmux name=mux ! shout2send mount=/telecaster_live_video.ogg port=8000 password=source2parisson ip=127.0.0.1 
 
